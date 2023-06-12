80166deea580aace06ae01ea16542a5671cc106f
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-12 17:46:29 +0530

    Add project files.

---
+++ CHAPTER04/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+char c ;
+
+Console.WriteLine("enter a alphabet");
+c = Convert.ToChar(Console.ReadLine()); 
+
+if  (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+{
+    Console.WriteLine("it is a vowel");
+}
+else
+{
+    Console.WriteLine("its not a vowel");
+}
